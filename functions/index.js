b119f8856bcabf4e1c84000063d62b912a41c05a
mikeheddes <mikeheddes@gmail.com>
2018-08-14 09:05:11 +0200

    Update firebase integration

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,7 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+
 const serverBundle = require('./build/server.bundle.js');
 const clientManifest = require('./build/client-manifest.json');
 const loadableStats = require('./build/react-loadable.json');
@@ -8,6 +11,8 @@ process.env.NODE_ENV = 'production';
 serverBundle.loadablePreload();
 
 exports.app = functions.https.onRequest((req, res) => {
+  res.set('Content-Type', 'text/html');
+  res.set('Cache-Control', 'max-age=300');
   serverBundle.default(req, res, {
     clientBundle: clientManifest['client.js'],
     vendorBundle: clientManifest['vendors.js'],
