892b3ba842c882b149b299767213915f8ff67360
mikeheddes <mikeheddes@gmail.com>
2018-11-18 01:52:29 +0100

    Add package and initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import styled from 'styled-components'
+
+const P = styled.p`
+  color: blue;
+`
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
+          <P>
             Edit <code>src/App.js</code> and save to reload.
-          </p>
+          </P>
           <a
             className="App-link"
             href="https://reactjs.org"
@@ -21,8 +23,8 @@ class App extends Component {
           </a>
         </header>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
