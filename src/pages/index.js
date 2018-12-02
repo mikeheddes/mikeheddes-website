0e04120f0467b889de004017a4bf8c38b587e306
mikeheddes <mikeheddes@gmail.com>
2018-12-02 14:36:33 +0100

    Add firebase and circleci config

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,14 @@
-export { default } from '../views/Home'
+import React from 'react'
+
+import Hero from '../views/Home/Hero'
+
+export default () => (
+  <React.Fragment>
+    <Hero />
+    {/* <LatestArticle
+      marginTop={{ xs: 'xr', md: 'md' }}
+      marginBottom={{ xs: 'xr', md: 'md' }}
+      />
+    <LatestMusic marginBottom={{ xs: 'xr', md: 'md' }} /> */}
+  </React.Fragment>
+)
