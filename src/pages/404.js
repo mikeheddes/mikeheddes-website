d986798bd2e495c6eda024ddd7e3ac94b44f81f1
mikeheddes <mikeheddes@gmail.com>
2019-01-01 23:40:49 +0100

    Add 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,18 +1,58 @@
 import React from 'react'
 import Helmet from 'react-helmet'
+import styled, { ThemeProvider } from 'styled-components'
 
 import Section from '../components/Section'
+import Link from '../components/Link'
 import Box from '../components/Box'
+import P from '../components/Paragraph'
+import { fluidFont } from '../styles/mixins'
+import { size } from '../styles'
 
-const About = () => (
+const Main = styled(Section)`
+  min-height: calc(100vh - ${size.footerHeight});
+`
+
+const MegaHeader = styled.h1`
+  text-align: center;
+  font-weight: 700;
+  ${fluidFont(120, 300)};
+  color: ${({ theme }) => theme.surface};
+`
+
+const themeColor = 'blue'
+const setTheme = theme => {
+  return {
+    ...theme,
+    link: theme[themeColor],
+    surface: theme.surfaceColors[themeColor],
+  }
+}
+
+const NotFound = () => (
   <React.Fragment>
     <Helmet>
       <title>Page Not Found</title>
     </Helmet>
-    <Section>
-      <Box width="text" marginLeft="auto" marginRight="auto" />
-    </Section>
+    <ThemeProvider theme={setTheme}>
+      <Main display="flex" justifyContent="center" direction="column">
+        <MegaHeader>404</MegaHeader>
+        <Box
+          width="text"
+          marginLeft="auto"
+          marginRight="auto"
+          marginTop="xm"
+          marginBottom="xm"
+          textAlign="center"
+        >
+          <P size="md">
+            Sorry the page you are looking for does not exist. You could start
+            at <Link to="/">the home page</Link>.
+          </P>
+        </Box>
+      </Main>
+    </ThemeProvider>
   </React.Fragment>
 )
 
-export default About
+export default NotFound
