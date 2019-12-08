9e08e0cc5086add27bf1c41e9f03cd8480950e1e
mikeheddes <mikeheddes@gmail.com>
2019-12-08 17:48:16 +0100

    Change file name to resolve case conflict

--- src/shared/page-wrapper.js
+++ src/shared/page-wrapper.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { MDXProvider } from '@mdx-js/react'
-import GlobalStyles from '../styles/global-styles'
-import Theme from '../styles/theme'
+import GlobalStyles from '../styles/global'
+import Theme from '../styles/themeWrapper'
 import MetaTags from './meta-tags'
 import Markdown from './markdown'
 
