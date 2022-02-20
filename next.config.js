9126f6bb16f63c82234bdd94df7cc1544214d5e4
mikeheddes <mikeheddes@gmail.com>
2022-02-19 16:14:53 -0800

    Update dependencies

--- next.config.js
+++ next.config.js
@@ -10,4 +10,7 @@ const withMDX = require("@next/mdx")({
 
 module.exports = withMDX({
   pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+  compiler: {
+    styledComponents: true,
+  },
 });
--- src/home/about.tsx
+++ src/home/about.tsx
@@ -2,11 +2,17 @@ import styled from "styled-components";
 
 import { contentWrapper, fluidFont } from "../styles";
 import { SectionName } from "./section";
+import { screen } from "../styles/breakpoints";
 
 const Section = styled.section`
   ${contentWrapper};
   margin-top: 30px;
   margin-bottom: 30px;
+
+  @media ${screen.sm} {
+    margin-top: 50px;
+    margin-bottom: 50px;
+  }
 `;
 
 const Text = styled.p`
